29147a123904ac93cdf4b42293bc1cb9a8376cef
Brian Werner <brianmw199@gmail.com>
2023-10-24 15:08:53 -0500

    Got atticus data

--- client.js
+++ client.js
@@ -48,10 +48,48 @@ console.log('array of employee data: ',  employees );
 
 // This function will calculate 1 employee's bonus!
 //
-function calculateIndividualEmployeeBonus( employee ) {  
+function calculateIndividualEmployeeBonus( array) {  
   // your logic here
+  // look at each objects of employees
+  // Look at employee ratings to figure out bonus
+   let bonuses = [];
+   let totalBonus;
+   let seniorityBonus;
+   
   
-  
-  // return new object with bonus results
+    let employee = array[0];
+    console.log(employee);
+    let employeeNumber = employee.employeeNumber;
+    console.log('expect 2405: ', employeeNumber);
+    let reviewRating = employee.reviewRating;
+    console.log('Expect 3', reviewRating);
+    let annualSalary = employee.annualSalary;
+    console.log('Expect 47000', annualSalary);
 
-}
+    if (reviewRating <= 2){
+      totalBonus = 0;
+    } else if (reviewRating === 3){
+      totalBonus = 0.04 * annualSalary;
+    } else if (reviewRating === 4){
+      totalBonus = 0.06 * annualSalary;
+    } else if (reviewRating === 5){
+      totalBonus = 0.10 * annualSalary;
+    }
+    //  If employeeNumber is 4 digits add 5% to bonus up to 13%
+    if (employeeNumber.length === 4 ){
+      seniorityBonus = 0.05 * annualSalary;
+      totalBonus += seniorityBonus;
+    }
+  
+//   // Look at annualsalary 
+//     // If income is greater that $65K subtract 1% down to 0
+    if (annualSalary > 65000){
+      totalBonus - (0.01 * annualSalary);
+    }
+  
+//   // return new object with bonus results
+
+  return totalBonus;
+
+};
+console.log(calculateIndividualEmployeeBonus(employees));
